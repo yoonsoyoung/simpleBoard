752c2b378677e5594c6f3406ef107d2d6866e7c4
yoonsoyoung <hummingsy@gmail.com>
2023-04-16 21:21:08 +0900

    Remove: Update spring boot gradle 재생성

--- backend/board/src/main/java/com/yoon/board/BoardApplication.java
+++ backend/board/src/main/java/com/yoon/board/BoardApplication.java
@@ -1,13 +1,13 @@
-package com.yoon.simpleboard;
+package com.yoon.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleboardApplication {
+public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleboardApplication.class, args);
+		SpringApplication.run(BoardApplication.class, args);
 	}
 
 }
