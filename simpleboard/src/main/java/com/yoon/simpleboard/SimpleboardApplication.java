5f96f598a0be4858a7b8dbce7f6e900829cdc02c
yoonsoyoung <hummingsy@gmail.com>
2023-04-16 19:07:07 +0900

    Feat: Add spring boot pjt init

---
+++ simpleboard/src/main/java/com/yoon/simpleboard/SimpleboardApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleboardApplication.class, args);
+	}
+
+}
