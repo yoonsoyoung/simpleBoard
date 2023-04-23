964ef078ac06c7aa31c1784405b6e66cb51075a3
yoonsoyoung <hummingsy@gmail.com>
2023-04-23 21:07:53 +0900

    Feat(member-crud): Update backend swagger dependency추가

--- backend/board/src/main/java/com/yoon/board/BoardApplication.java
+++ backend/board/src/main/java/com/yoon/board/BoardApplication.java
@@ -2,12 +2,18 @@ package com.yoon.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
+		SpringApplication app = new SpringApplication(BoardApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 
 }
