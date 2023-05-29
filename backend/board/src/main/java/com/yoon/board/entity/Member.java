4d88e35cf7b26d9fb85aedc4fb523873e36e4c20
yoonsoyoung <hummingsy@gmail.com>
2023-05-29 20:11:13 +0900

    Refac(member-crud): Update JPA ID 어노테이션 클래스 변경

--- backend/board/src/main/java/com/yoon/board/entity/Member.java
+++ backend/board/src/main/java/com/yoon/board/entity/Member.java
@@ -1,12 +1,12 @@
 package com.yoon.board.entity;
 
 import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
